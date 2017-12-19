c65dfe1fb42a1de1e7565848166bc969a0e4e752
TRINITE Benoit <benoit.trinite@gmail.com>
2017-12-19 13:10:37 +0100

    fix Serial link

--- arduino-pwm-proxy.ino
+++ arduino-pwm-proxy.ino
@@ -1,8 +1,11 @@
 
-#include "CurieTimerOne.h"
+//#include "CurieTimerOne.h"
+#include <SimpleTimer.h>
+// the timer object
+SimpleTimer timer;
 
-#define  PWM_RC_STEERING_PIN 5
-#define  PWM_RC_THROTTLE_PIN 11
+#define  PWM_RC_STEERING_PIN 11
+#define  PWM_RC_THROTTLE_PIN 5
  
 unsigned int pwm_throttle_value = 0;
 unsigned int pwm_steering_value = 0;
@@ -22,25 +25,38 @@ char buff [50] = {};
 void setup() {
 
   Serial.begin(115200);
+  Serial1.begin(115200);
   attachInterrupt(PWM_RC_THROTTLE_PIN, throttle_rising, RISING);
   attachInterrupt(PWM_RC_STEERING_PIN, steering_rising, RISING);
-  CurieTimerOne.start(OUTPUTLOOP, &timedCheckOutput);  // set timer and callback
+  timer.setInterval(50, timedCheckOutput);
+  //CurieTimerOne.start(OUTPUTLOOP, &timedCheckOutput);  // set timer and callback
 
 }
  
 void loop() {
-
+   timer.run();
 }
 
 void timedCheckOutput()
 {
+  boolean validData = false;
   if ((throttle_toggle == 0) || (steering_toggle == 0)) {
     sprintf(buff, "-1,-1,-1");
+    validData=true;
   } else {
-    sprintf(buff, "%d,%d,%d", pwm_throttle_value, pwm_steering_value, freq_value);
+    if (pwm_steering_value > 900 && 
+        pwm_steering_value < 2100 &&
+        pwm_throttle_value > 900 && 
+        pwm_throttle_value < 2200) {
+          sprintf(buff, "%d,%d,%d", pwm_steering_value, pwm_throttle_value, freq_value);    
+          validData=true;
+        }
   }
- 
-  Serial.println(buff);
+
+  if (validData) {
+    Serial.println(buff);    
+    Serial1.println(buff);    
+  }
   throttle_toggle = 0;
   steering_toggle = 0;  
 }
