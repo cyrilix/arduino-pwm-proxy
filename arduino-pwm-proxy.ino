ad80ba35bc7a605535a6647545bde0ab07240220
TRINITE Benoit <benoit.trinite@gmail.com>
2017-12-30 12:06:37 +0100

    add ts

--- arduino-pwm-proxy.ino
+++ arduino-pwm-proxy.ino
@@ -4,9 +4,9 @@
 // the timer object
 SimpleTimer timer;
 
-#define  PWM_RC_STEERING_PIN 11
-#define  PWM_RC_THROTTLE_PIN 5
- 
+#define  PWM_RC_STEERING_INPUT_PIN 11
+#define  PWM_RC_THROTTLE_INPUT_PIN 5
+
 unsigned int pwm_throttle_value = 0;
 unsigned int pwm_steering_value = 0;
 unsigned int freq_value = 0;
@@ -26,8 +26,8 @@ void setup() {
 
   Serial.begin(115200);
   Serial1.begin(115200);
-  attachInterrupt(PWM_RC_THROTTLE_PIN, throttle_rising, RISING);
-  attachInterrupt(PWM_RC_STEERING_PIN, steering_rising, RISING);
+  attachInterrupt(PWM_RC_THROTTLE_INPUT_PIN, throttle_rising, RISING);
+  attachInterrupt(PWM_RC_STEERING_INPUT_PIN, steering_rising, RISING);
   timer.setInterval(50, timedCheckOutput);
   //CurieTimerOne.start(OUTPUTLOOP, &timedCheckOutput);  // set timer and callback
 
@@ -39,16 +39,23 @@ void loop() {
 
 void timedCheckOutput()
 {
+  static int lastTx = 0;
+  int now = millis();
+  if ((now-lastTx) > 70) {
+    sprintf(buff, "Realtime issue : %d %d", now, lastTx);
+    Serial.println(buff);
+  }
+  lastTx=now;
   boolean validData = false;
   if ((throttle_toggle == 0) || (steering_toggle == 0)) {
-    sprintf(buff, "-1,-1,-1");
+    sprintf(buff, "%d,-1,-1,-1", now);
     validData=true;
   } else {
     if (pwm_steering_value > 900 && 
         pwm_steering_value < 2100 &&
         pwm_throttle_value > 900 && 
         pwm_throttle_value < 2200) {
-          sprintf(buff, "%d,%d,%d", pwm_steering_value, pwm_throttle_value, freq_value);    
+          sprintf(buff, "%d,%d,%d,%d", now, pwm_steering_value, pwm_throttle_value, freq_value);    
           validData=true;
         }
   }
@@ -62,19 +69,19 @@ void timedCheckOutput()
 }
 
 void throttle_rising() {
-  attachInterrupt(PWM_RC_THROTTLE_PIN, throttle_falling, FALLING);
+  attachInterrupt(PWM_RC_THROTTLE_INPUT_PIN, throttle_falling, FALLING);
   prev_throttle_time = micros();
   throttle_toggle ++;
 }
 
 void steering_rising() {
-  attachInterrupt(PWM_RC_STEERING_PIN, steering_falling, FALLING);
+  attachInterrupt(PWM_RC_STEERING_INPUT_PIN, steering_falling, FALLING);
   prev_steering_time = micros();
   steering_toggle ++;
 }
 
 void throttle_falling() {
-  attachInterrupt(PWM_RC_THROTTLE_PIN, throttle_rising, RISING);
+  attachInterrupt(PWM_RC_THROTTLE_INPUT_PIN, throttle_rising, RISING);
   pwm_throttle_value = micros()-prev_throttle_time;
   freq_value = 1000000 / (micros()-prev_freq_time); 
   prev_freq_time = micros();
@@ -82,7 +89,7 @@ void throttle_falling() {
 }
 
 void steering_falling() {
-  attachInterrupt(PWM_RC_STEERING_PIN, steering_rising, RISING);
+  attachInterrupt(PWM_RC_STEERING_INPUT_PIN, steering_rising, RISING);
   pwm_steering_value = micros()-prev_steering_time;
   steering_toggle ++;
 }
